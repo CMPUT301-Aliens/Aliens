7a7f816f92ec99d5f71c2e0a7628568f9cf0343f
Edward Bui <eb@Edwards-MacBook-Air-2.local>
2026-09-24 19:04:44 -0600

    ''

--- app/src/main/java/com/example/aliens/Shape.kt
+++ app/src/main/java/com/example/aliens/Shape.kt
@@ -1,6 +1,7 @@
 package com.example.aliens
 
-abstract class Shape  {
-    var x : Int = 0
-    var y : Int = 0
+abstract class Shape {
+    var x: Int = 0
+    var y: Int = 0
+    val color: String = "green"
 }
